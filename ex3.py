ef8887859a9d1b3d12b5875ba586f5fe4f15a456
Eduardo Teiken <edteiken@gmail.com>
2026-03-23 21:34:24 -0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,8 +1,10 @@
+#valores das variaveis A e B
 A = 10
 B = 20
 
 # Trocar os valores das variáveis
 A, B = B, A
 
+#imprime valores ja trocados entre variaveis A e B
 print(f"Valor de A após troca: {A}")
 print(f"Valor de B após troca: {B}")
