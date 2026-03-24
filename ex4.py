85dae169cf16558461fce57dbab5513c46365845
Eduardo Teiken <edteiken@gmail.com>
2026-03-23 21:34:59 -0300

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,9 @@
+#variaveis A e B
+A = 10
+B = 20
+
+#troca de valores das variaveia A e B
+A, B = B, A
+
+#imprime os valores invertidos
+print(f"Valor das duas variaveis após troca: a={A}, b={B}")
---
+++ ex5.py
@@ -0,0 +1,7 @@
+a = 6
+b = 7
+print(a,b)
+c = a
+a = b
+b = c
+print(a,b)
