d0b8ac82939cd3bde18ae98e2e361bd7ec679d08
Eduardo Teiken <edteiken@gmail.com>
2026-05-11 20:31:58 -0300

    Add files via upload

---
+++ vetores-1.py
@@ -0,0 +1,11 @@
+l = []
+print(l)
+l1 = [0] * 5 #0,0,0,0,0
+#l = [0 * 5] #0
+print(l1)
+l2 = [1,2,3,4,5]
+print(l2[5]) #l2[5] #IndexError: list index out of range
+l3 = ['a','b','c',]
+print(l3[2])
+l4 = list("tsi") #['t','s','i']
+print(l4)
